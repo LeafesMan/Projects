2ec89422e8c1ecf03490945e4c4a2b3a40ef0a84
Ian Ford <89503834+LeafesMan@users.noreply.github.com>
2023-10-15 12:32:46 -0400

    Added 3D Lerped Noise
    Added Function for Lerped 3D noise, Fixed issue where Lerped Noise would produce garbage values if any input position was negative

--- Noise/LeafNoise.cs
+++ Noise/LeafNoise.cs
@@ -1,22 +1,26 @@
-/*************************************************
+/*
  *  Author: Ian
  *
  *  Project: General Utility
  *
  *  Date: 8/5/22  
  *  
- *  Various Noise and Random methods based on Squirrel3 Noise
- *************************************************/
+ *  Various Noise and Random methods built on the back of Squirrel3 Noise
+ */
+
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.UIElements;
 
 public class LeafNoise
 {
+    #region Random
     private static uint seed = (uint)System.DateTime.Now.Millisecond;
-    
+
 
     //A series of random methods each randomizes the static seed when used
+
+    public static uint GetSeed() => seed;
+    public static void SetSeed(uint newSeed) => seed = newSeed;
 
     ///<returns>Random roll against P(probability).</returns>
     public static bool RandomChance(float probability)
@@ -34,11 +38,11 @@ public class LeafNoise
         return seed;
     }
     /// <summary>Returns a random float [min, max]. (Inclusive)</summary>
-    public static float Random(float min, float max) => NoiseToRange(Random(), new Vector2(min,max));
+    public static float Random(float min, float max) => UIntToRange(Random(), min,max);
     /// <summary>Returns a random float [range.x, range.y]. (Inclusive)</summary>
     public static float Random(Vector2 range) => Random(range.x, range.y);
     /// <summary>Returns a random integer [min, max]. (Inclusive)</summary>
-    public static int Random(int min, int max) => NoiseToRange(Random(), new Vector2Int(min, max));
+    public static int Random(int min, int max) => UIntToRange(Random(), min, max);
     /// <summary>Returns a random integer [range.x, range.y]. (Inclusive)</summary>
     public static int Random(Vector2Int range) => Random(range.x, range.y);
 
@@ -134,10 +138,12 @@ public class LeafNoise
         public float GetWeight(); 
     }
     #endregion
+    #endregion
     #region Noise
-    /// <summary>Squirel3 1D Noise.</summary>
+    #region Squirrel3 Functions
+    /// <summary>Returns 1D Noise given a position and a seed</summary>
     public static uint Noise(int position, uint seed)
-    {
+    {   //Squirrel3 1D Noise Function
         uint bigP1 = 0x68E31DA4;
         uint bigP2 = 0xB5297A4D;
         uint bigP3 = 0x1B56C4E9;
@@ -152,18 +158,19 @@ public class LeafNoise
         mangled ^= (mangled >> 8);
         return mangled;
     }
-    /// <summary>Squirel3 2D Noise.</summary>
+    /// <summary>Returns 2D Noise given a position and a seed</summary>
     public static uint Noise(int x, int y, uint seed) => Noise(x + y * 198491317, seed);
+    /// <summary>Returns 3D Noise given a position and a seed</summary>
+    public static uint Noise(int x, int y, int z, uint seed) => Noise(x + y * 198491317 + z * 6542989, seed);
+    #endregion
     /// <summary>Returns a linear interpolation between 1D noise values.</summary>
     public static uint Noise(float position, uint seed)
     {
-        uint floorNoise = Noise(Mathf.FloorToInt(position), seed);
-        uint ceilNoise = Noise(Mathf.CeilToInt(position), seed);
+        uint floorNoise = Noise(Floor(position), seed);
+        uint ceilNoise = Noise(Ceil(position), seed);
 
-
-        return Lerp(floorNoise, ceilNoise, position % 1);
+        return Lerp(floorNoise, ceilNoise, GetRawDecimal(position));
     }
-    public static float Noise(float position, uint seed, Vector2 range) => NoiseToRange(Noise(position, seed), range);
     /// <summary>Returns a linear interpolation between 2D noise values.</summary>
     public static uint Noise(float x, float y, uint seed)
     {
@@ -171,31 +178,63 @@ public class LeafNoise
         //uint x1toX2YFloorLerped = Noise(Noise(x + Mathf.Floor(y * 198491317), seed) + x % 1, seed);
         //uint x1toX2YCeilLerped  = Noise(Noise(x + Mathf.Ceil(y * 198491317), seed) + x % 1, seed);
 
-        uint floorNoise = Noise(Mathf.FloorToInt(x), Mathf.FloorToInt(y), seed);
-        uint ceilNoise =  Noise(Mathf.CeilToInt(x), Mathf.FloorToInt(y), seed);
-        uint x1toX2YFloorLerped = Lerp(floorNoise, ceilNoise, x % 1);
+        uint floorNoise = Noise(Floor(x), Floor(y), seed);
+        uint ceilNoise =  Noise(Ceil(x), Floor(y), seed);
+        uint x1toX2YFloorLerped = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
 
 
-        floorNoise =      Noise(Mathf.FloorToInt(x), Mathf.CeilToInt(y), seed);
-        ceilNoise =       Noise(Mathf.CeilToInt(x), Mathf.CeilToInt(y), seed);
-        uint x1toX2YCeilLerped = Lerp(floorNoise, ceilNoise, x % 1);
+        floorNoise =      Noise(Floor(x), Ceil(y), seed);
+        ceilNoise =       Noise(Ceil(x), Ceil(y), seed);
+        uint x1toX2YCeilLerped = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
 
         //Return Interpolation between those two values
-        return Lerp(x1toX2YFloorLerped, x1toX2YCeilLerped, y % 1);
+        return Lerp(x1toX2YFloorLerped, x1toX2YCeilLerped, GetRawDecimal(y));
     }
-    public static float Noise(float x, float y, uint seed, Vector2 range) => NoiseToRange(Noise(x, y, seed), range);
+    public static uint Noise(float x, float y, float z, uint seed)
+    {
+        uint floorNoise = Noise(Floor(x), Floor(y), Floor(z), seed);
+        uint ceilNoise = Noise(Ceil(x), Floor(y), Floor(z), seed);
+        uint valAtYFloor = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
+
+        floorNoise = Noise(Floor(x), Ceil(y), Floor(z), seed);
+        ceilNoise = Noise(Ceil(x), Ceil(y), Floor(z), seed);
+        uint valAtYCeil = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
+
+        uint valAtZFloor = Lerp(valAtYFloor, valAtYCeil, GetRawDecimal(y));
+
+
+        floorNoise = Noise(Floor(x), Floor(y), Ceil(z), seed);
+        ceilNoise = Noise(Ceil(x), Floor(y), Ceil(z), seed);
+        valAtYFloor = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
+
+        floorNoise = Noise(Floor(x), Ceil(y), Ceil(z), seed);
+        ceilNoise = Noise(Ceil(x), Ceil(y), Ceil(z), seed);
+        valAtYCeil = Lerp(floorNoise, ceilNoise, GetRawDecimal(x));
+
+        uint valAtZCeil = Lerp(valAtYFloor, valAtYCeil, GetRawDecimal(y));
+
+
+        return Lerp(valAtZFloor, valAtZCeil, GetRawDecimal(z));
+    }
     #endregion
     #region Util
     /// <summary>Converts noise into a value [range.x, range.y]. (Inclusive)</summary>
-    public static float NoiseToRange(uint noise, Vector2 range) => range.x + (float)noise / uint.MaxValue * (range.y - range.x);
+    public static float UIntToRange(uint noise, float min, float max) => min + (float)noise / uint.MaxValue * (max - min);
     /// <summary>Converts noise into a value [range.x, range.y]. (Inclusive)</summary>
-    public static int NoiseToRange(uint noise, Vector2Int range)
+    public static int UIntToRange(uint noise, int min, int max)
     {
-        if (noise == uint.MaxValue) return range.y;
-        else return Mathf.FloorToInt(range.x + (float)noise / uint.MaxValue * (range.y + 1 - range.x));
+        if (noise == uint.MaxValue) return max;
+        else return Floor(min + (float)noise / uint.MaxValue * (max + 1 - min));
     }
 
     /// <summary> uint Lerp </summary>
-    public static uint Lerp(uint from, uint to, float lerpPercent) => (uint)( (1-lerpPercent) * from   +   lerpPercent * to);
+    static uint Lerp(uint from, uint to, float lerpPercent) => (uint)( (1-lerpPercent) * from   +   lerpPercent * to);
+    static int Ceil(float num)
+    {   //Returns num rounding up relative to the absolute numue of num
+        if (num % 1 == 0) return (int)num;
+        else return (int)(num + Mathf.Sign(num));
+    }
+    static int Floor(float num) => (int)num;
+    static float GetRawDecimal(float num) => Mathf.Abs(num % 1);
     #endregion
 }
