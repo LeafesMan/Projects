47af24d7b508ee75d0914f14a7b11bfd079c4f17
Ian Ford <89503834+LeafesMan@users.noreply.github.com>
2023-10-19 16:04:18 -0400

    Update VariableAudioClip.cs

--- LeafAudio/VariableAudioClip.cs
+++ LeafAudio/VariableAudioClip.cs
@@ -12,21 +12,38 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Audio/Variable Clip", fileName = "VA_NewVariableClip")]
 public class VariableAudioClip : PlayableClip
 {
-    public AudioClip clip;
+    public AudioClipSpecs[] clips;
 
-    [SerializeField,Slider(0, 1)] private Vector2 volumeRange;
-    [SerializeField,Slider(0, 3)] private Vector2 pitchRange;
-    private float GetRandVolume() => Random.Range(volumeRange.x, volumeRange.y);
-    private float GetRandPitch() => Random.Range(pitchRange.x, pitchRange.y);
+    [System.Serializable]
+    public struct AudioClipSpecs 
+    {
+        public AudioClip clip;
+        [SerializeField, Slider(0, 1)] private Vector2 volumeRange;
+        [SerializeField, Slider(0, 3)] private Vector2 pitchRange;
+        public float GetRandVolume() => Random.Range(volumeRange.x, volumeRange.y);
+        public float GetRandPitch() => Random.Range(pitchRange.x, pitchRange.y);
+    }
+
+    AudioClipSpecs GetRandomSpecs() => clips[Random.Range(0, clips.Length)];
     public override void Test()
     {
         AudioSource source = GameObject.Find("Main Camera").GetComponent<AudioSource>();
-        source.clip = clip;
-        source.volume = GetRandVolume();
-        source.pitch = GetRandPitch();
+        AudioClipSpecs toPlay = GetRandomSpecs();
+        source.clip = toPlay.clip;
+        source.volume = toPlay.GetRandVolume();
+        source.pitch = toPlay.GetRandPitch();
         source.Play();
     }
 
-    public override void Play() => AudioHandler.PlayClipDelegate?.Invoke(clip, GetRandVolume(), GetRandPitch());
-    public override void Play(Vector3 pos) => AudioHandler.PlayPositionalClipDelegate?.Invoke(clip, GetRandVolume(), GetRandPitch(), pos);
+    public override void Play()
+    {
+        AudioClipSpecs toPlay = GetRandomSpecs();
+        AudioHandler.PlayClip(toPlay.clip, toPlay.GetRandVolume(), toPlay.GetRandPitch());
+    }
+    public override void Play(Vector3 pos)
+    {
+        AudioClipSpecs toPlay = GetRandomSpecs();
+
+        AudioHandler.PlayClipPositional(toPlay.clip, toPlay.GetRandVolume(), toPlay.GetRandPitch(), pos);
+    }
 }
